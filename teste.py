0b7d8de62749f6292eae70b9f0adc39c02b4596f
Auridebson Lima <auridebson@vutus.com>
2024-01-19 12:00:28 -0300

    Update

--- teste.py
+++ teste.py
@@ -1,3 +1,15 @@
 import random
 from db import *
+import re
 
+while True:
+    cpf = input("Digite o CPF: ")
+    cpff = re.sub(r'[^0-9]', '', cpf)
+    print(type(cpf))
+    if len(cpf) < 11 and len(cpf) > 14:
+        print("Digite um CPF válido!")
+    elif cpf is not int:
+        print("Digite um número inteiro válido!")
+    else:
+        int(cpf)
+        break
